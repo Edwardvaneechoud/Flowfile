d66086510eb69de5569d6e4df3a1a355864b0745
edwardvaneechoud <evaneechoud@gmail.com>
2025-08-09 20:42:00 +0200

    Fixing issue with complex expressions in group by

--- docs/for-developers/how-it-works.py
+++ docs/for-developers/how-it-works.py
@@ -122,3 +122,33 @@ def flow_frame_implementation():
     WITH_COLUMNS:
     [[(col("quantity")) * (col("price"))].alias("total")]
     DF ["id", "region", "quantity", "price"]; PROJECT["region", "quantity", "price"] 3/4 COLUMNS"""
+
+def test_another_implementation():
+    raw_data = [
+        {"id": 1, "region": "North", "quantity": 10, "price": 150},
+        {"id": 2, "region": "South", "quantity": 5, "price": 300},
+        {"id": 3, "region": "East", "quantity": 8, "price": 200},
+        {"id": 4, "region": "West", "quantity": 12, "price": 100},
+        {"id": 5, "region": "North", "quantity": 20, "price": 250},
+        {"id": 6, "region": "South", "quantity": 15, "price": 400},
+        {"id": 7, "region": "East", "quantity": 18, "price": 350},
+        {"id": 8, "region": "West", "quantity": 25, "price": 500},]
+
+    from flowfile_core.flowfile.flow_graph import FlowGraph
+    graph: FlowGraph = ff.create_flow_graph()
+    breakpoint()
+    df_1 = ff.FlowFrame(
+    raw_data, flow_graph=graph)
+    df_2 = df_1.with_columns(flowfile_formulas=['[quantity] * [price]'], output_column_names=["total"])
+    df_3 = df_2.filter(flowfile_formula="[total]>1500")
+    df_4 = df_3.group_by(['region']).agg([
+        ff.col("total").sum().alias("total_revenue"),
+        ff.col("total").mean().alias("total_quantity"),
+    ])
+    print(df_4.get_node_settings().setting_input)
+    # flow_id=9090 node_id=5 cache_results=False pos_x=200.0 pos_y=200.0 is_setup=True description='Aggregate after grouping by "region"' user_id=None is_flow_output=False depending_on_id=4 groupby_input=GroupByInput(agg_cols=[AggColl(old_name='region', agg='groupby', new_name='region', output_type=None), AggColl(old_name='total', agg='sum', new_name='total_revenue', output_type=None), AggColl(old_name='total', agg='mean', new_name='total_quantity', output_type='Float64')])
+    df_5 = df_3.collect().group_by([(ff.col("region").str.to_uppercase() + ff.lit("test"))]).agg(
+                         ff.col("total").sum().alias("total_revenue"),
+                         ff.col("total").mean().alias("total_quantity"),
+                         )
+    df_5.collect()
