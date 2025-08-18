d98ad7bdfac6909f9a0d3898f3e48438295a4a30
Bernardo Fernandes <bernardo.n.fernandes@gmail.com>
2025-08-18 12:18:21 +0100

    Changes to node ordering of print_tree

--- flowfile_core/flowfile_core/flowfile/flow_graph.py
+++ flowfile_core/flowfile_core/flowfile/flow_graph.py
@@ -227,6 +227,11 @@ class FlowGraph:
         elif input_flow is not None:
             self.add_datasource(input_file=input_flow)
 
+        self.skip_nodes = [node for node in self.nodes if not node.is_correct]
+        self.skip_nodes.extend([lead_to_node for node in self.skip_nodes for lead_to_node in node.leads_to_nodes])
+        self.execution_order = determine_execution_order(all_nodes=[node for node in self.nodes if
+        node not in self.skip_nodes],flow_starts=self._flow_starts+self.get_implicit_starter_nodes())
+
     def add_node_promise(self, node_promise: input_schema.NodePromise):
         """Adds a placeholder node to the graph that is not yet fully configured.
 
@@ -1545,11 +1550,6 @@ class FlowGraph:
             self.end_datetime = None
             self.latest_run_info = None
             self.flow_logger.info('Starting to run flowfile flow...')
-            self.skip_nodes = [node for node in self.nodes if not node.is_correct]
-            self.skip_nodes.extend([lead_to_node for node in self.skip_nodes for lead_to_node in node.leads_to_nodes])
-            self.execution_order = determine_execution_order(all_nodes=[node for node in self.nodes if
-                                                                   node not in self.skip_nodes],
-                                                        flow_starts=self._flow_starts+self.get_implicit_starter_nodes())
 
             skip_node_message(self.flow_logger, self.skip_nodes)
             execution_order_message(self.flow_logger, self.execution_order)
