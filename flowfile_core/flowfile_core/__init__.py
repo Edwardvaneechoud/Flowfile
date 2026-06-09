2db58048e9af9a370e9181fd0d34f78fe1a75404
Edward van Eechoud <41021650+Edwardvaneechoud@users.noreply.github.com>
2026-06-09 08:18:51 +0200

    Bump version (#498)

--- flowfile_core/flowfile_core/__init__.py
+++ flowfile_core/flowfile_core/__init__.py
@@ -22,6 +22,6 @@ class ServerRun:
 try:
     __version__ = version("Flowfile")
 except PackageNotFoundError:
-    __version__ = "0.11.2"
+    __version__ = "0.11.3"
 
 flow_file_handler = FlowfileHandler()
